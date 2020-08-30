ed733944ed0d3781f8dadab3f0b6b6553b02eace
nikitashrestha <nik.stha81@gmail.com>
2020-08-30 14:03:59 +0545

    minor-fix: Remove basepath

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,26 +7,23 @@ const Header = (props) => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
-        <a href={`${process.env.PUBLIC_URL}/`} className="navbar-brand">
+        <a href="/" className="navbar-brand">
           {branding}
         </a>
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to={`${process.env.PUBLIC_URL}/`} className="nav-link">
+              <Link to="/" className="nav-link">
                 <i className="fas fa-home" /> Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link
-                to={`${process.env.PUBLIC_URL}/contact/add`}
-                className="nav-link"
-              >
+              <Link to="/contact/add" className="nav-link">
                 <i className="fas fa-plus" /> Add
               </Link>
             </li>
             <li className="nav-item">
-              <Link to={`${process.env.PUBLIC_URL}/about`} className="nav-link">
+              <Link to="/about" className="nav-link">
                 <i className="fas fa-question" /> About
               </Link>
             </li>
