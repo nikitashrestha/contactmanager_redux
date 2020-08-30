1c4bdaed756d2ea321f423f1e6483edfd72043c6
nikitashrestha <nik.stha81@gmail.com>
2020-08-30 11:55:09 +0545

    Updates

---
+++ precache-manifest.1b2f0304bb922a2828476970566777c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9ae90f49c97585f232b",
+    "url": "/contactmanager_redux/static/css/main.7077f9fe.chunk.css"
+  },
+  {
+    "revision": "e9ae90f49c97585f232b",
+    "url": "/contactmanager_redux/static/js/main.e9ae90f4.chunk.js"
+  },
+  {
+    "revision": "d4e653e3999b69b03284",
+    "url": "/contactmanager_redux/static/css/1.df95de14.chunk.css"
+  },
+  {
+    "revision": "d4e653e3999b69b03284",
+    "url": "/contactmanager_redux/static/js/1.d4e653e3.chunk.js"
+  },
+  {
+    "revision": "af368d9d5ac399d10ac9",
+    "url": "/contactmanager_redux/static/js/runtime~main.af368d9d.js"
+  },
+  {
+    "revision": "988041dd11f671ba2f3163604885fda8",
+    "url": "/contactmanager_redux/index.html"
+  }
+];
