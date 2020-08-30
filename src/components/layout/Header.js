4d3152a4b009b0782398a8695c0de7f1e48e554f
nikitashrestha <nik.stha81@gmail.com>
2020-08-30 14:16:06 +0545

    minor-fix: Update basepath

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-import config from "./config";
+import config from "./../../config";
 
 const Header = (props) => {
   const { branding } = props;
